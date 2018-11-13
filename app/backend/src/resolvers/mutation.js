30242599a4dfd2216ede6b99cda623f541a77035
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-11-13 16:46:12 -0600

    mutation and query for recipes

--- app/backend/src/resolvers/mutation.js
+++ app/backend/src/resolvers/mutation.js
@@ -1,3 +1,13 @@
-const Mutation = {};
+const Mutation = {
+  async createRecipe(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const recipe = await ctx.db.mutation.createRecipe({
+      data: {
+        ...args,
+      },
+    }, info);
+    return recipe;
+  },
+};
 
 module.exports = Mutation;
--- app/backend/src/resolvers/query.js
+++ app/backend/src/resolvers/query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  recipes: forwardTo('db'),
+  // async recipes(parent, args, ctx, info) {
+  //   const recipes = await ctx.db.query.recipes();
+  //   console.log('recipes here');
+  //   return recipes;
+  // },
+};
 
 module.exports = Query;
